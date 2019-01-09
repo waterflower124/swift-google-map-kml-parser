f29a35c5515000f52f4e6404edde562e9078e085
dev1ninjweb <46208685+dev1ninjweb@users.noreply.github.com>
2019-01-09 18:30:05 +0800

    initial

---
+++ DogOutMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DogOutMap
+//
+//  Created by Water Flower on 2019/1/9.
+//  Copyright © 2019 Water Flower. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ DogOutMapTests/DogOutMapTests.swift
@@ -0,0 +1,34 @@
+//
+//  DogOutMapTests.swift
+//  DogOutMapTests
+//
+//  Created by Water Flower on 2019/1/9.
+//  Copyright © 2019 Water Flower. All rights reserved.
+//
+
+import XCTest
+@testable import DogOutMap
+
+class DogOutMapTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
